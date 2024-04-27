e5a806603482cffd4154d245cdc9d08e2207b739
Aditya Kumar Saha <113898640+asaha12@users.noreply.github.com>
2024-04-27 13:23:58 +0600

    Add files via upload

--- ust/src/test/java/com/ad/pages/LoginPage.java
+++ ust/src/test/java/com/ad/pages/LoginPage.java
@@ -1,16 +1,23 @@
 package com.ad.pages;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.ad.basedriver.PageDriver;
+import com.ad.utilities.GetScreenShot;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 public class LoginPage {
+	ExtentTest test;
 	
-	public LoginPage()
+	public LoginPage(ExtentTest test)
 	{
 		PageFactory.initElements(PageDriver.getCurrentDriver(), this);
+		this.test = test;
 			
 	}
 	
@@ -24,15 +31,73 @@ public class LoginPage {
 	WebElement LoginButton;
 	
 	
-	public void Login() throws InterruptedException
+	// Pass Case
+	public void passCase(String message) {
+		test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>" + message + "</b></p>");
+	}
+
+	public void passCaseWithSC(String message, String scName) throws IOException {
+		test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>" + message + "</b></p>");
+		String screenShotPath = GetScreenShot.capture(PageDriver.getCurrentDriver(), "" + scName + "");
+		String dest = System.getProperty("user.dir") + "\\screenshots\\" + "" + scName + ".png";
+		test.pass(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+	}
+
+	// Fail
+	public void failCase(String message, String scName) throws IOException {
+		test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>" + message + "</b></p>");
+		Throwable t = new InterruptedException("Exception");
+		test.fail(t);
+		String screenShotPath = GetScreenShot.capture(PageDriver.getCurrentDriver(), "" + scName + "");
+		String dest = System.getProperty("user.dir") + "\\screenshots\\" + "" + scName + ".png";
+		test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+		PageDriver.getCurrentDriver().quit();
+	}
+
+	
+	public void Login() throws IOException
 	{
-		userEmail.sendKeys("codemen.turny@gmail.com");
-		userPass.sendKeys("TTT@wd123!!");
-		LoginButton.click();
-		Thread.sleep(50000);
+				try {
+					test.info("Please enter your email.");
+					if (userEmail.isDisplayed()) {
+						userEmail.sendKeys("codemen.turny@gmail.com");
+						passCase("You have entered your username");
+						Thread.sleep(2000);
+						try {
+							test.info("Please enter your password.");
+							if (userPass.isDisplayed()) {
+								userPass.sendKeys("TTT@wd123!!");
+								passCase("You have entered your password");
+								Thread.sleep(2000);
+								try {
+									test.info("Please click on the Login Button.");
+									if (LoginButton.isDisplayed()) {
+										LoginButton.click();
+										Thread.sleep(20000);
+										passCaseWithSC("Login Successful", "login_pass");
+									}
+								} catch (Exception e) {
+									failCase("Submit button was not locateable.", "submit_fail");
+								}
+							}
+						} catch (Exception e) {
+							failCase("Password was not locateable. Please check the error message.", "pass_fail");
+
+						}
+					}
+				} catch (Exception e) {
+					failCase("User name was not locateable. Please check the error message.", "username_fail");
+				}
+			}
 		
+		
+		
+		
+		
+		
+		
+
 		
 	}
 	
 
-}
--- ust/src/test/java/com/ad/tests/LoginTest.java
+++ ust/src/test/java/com/ad/tests/LoginTest.java
@@ -1,28 +1,46 @@
 package com.ad.tests;
 
+import java.io.IOException;
+
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.ad.basedriver.BaseDriver;
 import com.ad.basedriver.PageDriver;
 import com.ad.pages.LoginPage;
+import com.ad.utilities.ExtentFactory;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
 public class LoginTest extends BaseDriver {
+	
+	ExtentReports report;
+	ExtentTest parentTest;
+	ExtentTest childTest;
 
 	@BeforeClass
 	public void start() throws InterruptedException
 	{
 		PageDriver.getCurrentDriver().get(url);
 		Thread.sleep(3000);
+		report = ExtentFactory.getInstance();
+		parentTest = report.createTest("<p style=\"color:#FF6000; font-size:20px\"><b>GroundFloor v2 Login</b></p>").assignAuthor("QA TEAM").assignDevice("Windows");
 		
 	}
 	
 	@Test
-	public void loginTest() throws InterruptedException
+	public void loginTest() throws IOException
 	{
-		LoginPage loginPage = new LoginPage();
+		childTest =parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>Login</b></p>");
+		LoginPage loginPage = new LoginPage(childTest);
 		loginPage.Login();
 		
+	}
+	
+	@AfterClass
+	public void report() {
+		report.flush();
 	}
 	
 	
